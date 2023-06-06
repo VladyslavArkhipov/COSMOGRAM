4ddda0dec44ae114983740247d2863684f93af59
Vlad <arkhipovvladyslav@gmail.com>
2023-06-06 16:56:58 +0200

    changes

--- photos-app-main/js/main.js
+++ photos-app-main/js/main.js
@@ -5,7 +5,7 @@ const commentsMessage = [
   `Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.`,
   `Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.`,
   `Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?`,
-];
+]; //массив с текстом комментария
 
 const commentsUserName = [
   "Артем",
@@ -22,26 +22,24 @@ const commentsUserName = [
   "Анастасiя",
   "Oлена",
   "Катерина",
-];
+]; //массив имен комментаторов
 
 const userPhotosDescriptions = [
   `Майстерно запечатлений момент, що розкриває весь спектр кольорів і емоцій.`,
   `Живописний кадр, де кожна деталь виблискує красою і пристрастю.`,
   `Витончений знімок, який вражає глибиною та насиченістю кольорів.`,
-  `Фотографія, що переносить глядача в чарівний світ, де кожен тон видає майстерність автора.
-    `,
+  `Фотографія, що переносить глядача в чарівний світ, де кожен тон видає майстерність автора.`,
   `Вишукана композиція, яка розкриває всю красу і неповторність моменту з витонченою грацією.`,
-  `Вразлива фотографія, що здатна перетворити звичайний кадр на справжню шедевральну мозаїку кольорів.
-    `,
+  `Вразлива фотографія, що здатна перетворити звичайний кадр на справжню шедевральну мозаїку кольорів.`,
   `Магічний знімок, де віртуозне використання кольорів створює неповторну атмосферу та розповідає цілу історію.`,
   `Вражаюча фотографія, яка здатна привернути увагу до найдрібніших деталей та перенести у світ незабутніх вражень.`,
   `Художнє полотно, на якому кожен краплинка кольору витає у повітрі і майстерно грає свою роль.`,
   `Очаровуючий знімок, де кольори стають головними героями і створюють неповторну симфонію естетики.`,
-];
+]; //массив текстовых описаний к фото
 
 const usersPhotos = new Array(25)
   .fill(null)
-  .map((el, i) => getUsersPhotosObject(i));
+  .map((el, i) => getUsersPhotosObject(i)); //массив объектов с фото пользователей
 
 function getUsersPhotosObject(i) {
   return {
@@ -51,7 +49,7 @@ function getUsersPhotosObject(i) {
     likes: createRandomLikesCount(),
     comments: getUsersComments(),
   };
-}
+} //объект для пользовательских фото
 
 function getUsersComments() {
   const usersComments = [];
@@ -69,7 +67,7 @@ function getUsersComments() {
       });
     }
   }
-  return usersComments;
+  return usersComments; //массив комментариев к фото
 }
 
 function createRandomLikesCount() {
@@ -78,7 +76,7 @@ function createRandomLikesCount() {
   return Math.floor(
     Math.random() * (maxLikesCount - minLikesCount) + minLikesCount
   );
-}
+} //случайное количество лайков
 
 function createRandomAvatar() {
   const minAvatarId = 1;
@@ -87,7 +85,7 @@ function createRandomAvatar() {
     Math.random() * (maxAvatarId - minAvatarId) + minAvatarId
   );
   return `img/avatar-${randomAvatarId}.svg`;
-}
+} //случайный путь к файлу с аватаром пользователя
 
 function createRandomCommentMessage() {
   const minCommentMessageId = 0;
@@ -97,7 +95,7 @@ function createRandomCommentMessage() {
       minCommentMessageId
   );
   return commentsMessage[randomCommentMessageId];
-}
+} //случайный комментарий из массива комментариев
 
 function createRandomCommentUserName() {
   const minCommentUserNameId = 0;
@@ -107,7 +105,7 @@ function createRandomCommentUserName() {
       minCommentUserNameId
   );
   return commentsUserName[randomCommentUserNameId];
-}
+} //случайное имя пользователя из массива имен
 
 function createRandomUserPhotosDescription() {
   const minUsersPhotoDescriptionId = 0;
@@ -117,12 +115,10 @@ function createRandomUserPhotosDescription() {
       minUsersPhotoDescriptionId
   );
   return userPhotosDescriptions[randomUserPhotosDescriptionId];
-}
+} //случайное описание к фото из массива описаний
 
 function createRandomCommentId() {
   const minId = 1;
   const maxId = 999;
   return Math.floor(Math.random() * (maxId - minId) + minId);
-}
-
-console.log(usersPhotos);
+} //случайный номер айди пользователя
