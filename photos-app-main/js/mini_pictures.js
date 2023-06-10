08bf23e8a2a48cd1c1f1ca084c2293e1add714d1
Vlad <arkhipovvladyslav@gmail.com>
2023-06-10 12:13:44 +0200

    changes

--- photos-app-main/js/mini_pictures.js
+++ photos-app-main/js/mini_pictures.js
@@ -3,13 +3,13 @@ import { userPhotos } from "./main.js"; //импортирую массив об
 const picturesBlock = document.querySelector(".pictures"); //нахожу блок в который вставлять фото
 const pictureTemplate = document.querySelector("#picture"); //нахожу шаблон с кодом для вставки фото
 
-for (let el of userPhotos) {
+userPhotos.forEach((photo) => {
   const picture = pictureTemplate.content.cloneNode(true);
   const image = picture.querySelector(".picture__img");
   const comments = picture.querySelector(".picture__comments");
   const likes = picture.querySelector(".picture__likes");
-  image.src = `${el.url}`;
-  comments.textContent = `${el.comments.length}`;
-  likes.textContent = `${el.likes}`;
+  image.src = `${photo.url}`;
+  comments.textContent = `${photo.comments.length}`;
+  likes.textContent = `${photo.likes}`;
   picturesBlock.append(picture);
-} //перебираю элементы массива для того, чтобы после клонирования содержимого шаблона, можно было указать необходимые данные внутри контента
+}); //перебираю элементы массива для того, чтобы после клонирования содержимого шаблона, можно было указать необходимые данные внутри контента
