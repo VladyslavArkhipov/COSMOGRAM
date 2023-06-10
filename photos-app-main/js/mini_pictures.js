50891f186d9b06b3b26bfc0de2c6fd1336f03681
Vlad <arkhipovvladyslav@gmail.com>
2023-06-10 13:41:41 +0200

    changes

--- photos-app-main/js/mini_pictures.js
+++ photos-app-main/js/mini_pictures.js
@@ -2,6 +2,7 @@
 import { userPhotos } from "./main.js"; //импортирую массив объектов с фото
 const picturesBlock = document.querySelector(".pictures"); //нахожу блок в который вставлять фото
 const pictureTemplate = document.querySelector("#picture"); //нахожу шаблон с кодом для вставки фото
+const documentFragment = document.createDocumentFragment(); //создаю фрагмент документа, в который буду добавлять элементы фото
 
 userPhotos.forEach((photo) => {
   const picture = pictureTemplate.content.cloneNode(true);
@@ -11,5 +12,7 @@ userPhotos.forEach((photo) => {
   image.src = `${photo.url}`;
   comments.textContent = `${photo.comments.length}`;
   likes.textContent = `${photo.likes}`;
-  picturesBlock.append(picture);
+  documentFragment.appendChild(picture); //все новые элементы добавляю в фрагмент документа который затем буду вставлять в блок для фото
 }); //перебираю элементы массива для того, чтобы после клонирования содержимого шаблона, можно было указать необходимые данные внутри контента
+
+picturesBlock.appendChild(documentFragment); //добавляю фрагмент документа в блок для фото для вывода всех фото
