73cc4b3e2c8ec7720cf1fb4b9d63c411a0f6dc44
Vlad <arkhipovvladyslav@gmail.com>
2023-07-03 09:48:28 +0200

    changes

--- photos-app-main/js/openBigPictureWindow.js
+++ photos-app-main/js/openBigPictureWindow.js
@@ -3,58 +3,123 @@ import { userPhotos } from "./main.js"; //импортирую массив об
 
 const bigPictureSection = document.querySelector(".big-picture"); //Нахожу секцию для показа большого фото
 const body = document.querySelector("body"); //Нахожу тег body
+const image = bigPictureSection.querySelector("img"); //Нахожу тег img внутри секции с большим фото
+const description = bigPictureSection.querySelector(".social__caption"); //Нахожу тег с классом social__caption внутри секции с большим фото
+const likesCount = bigPictureSection.querySelector(".likes-count"); //Нахожу тег с классом likes-count внутри секции с большим фото
+
+const commentsList = bigPictureSection.querySelector(".social__comments"); //Нахожу тег для вписывания элементов списка
+const commentsMaxCount = bigPictureSection.querySelector(".comments-count"); //Нахожу тег с классом comments-count внутри секции с большим фото
+const commentsShown = document.querySelector(".comments-shown");
+const commentsCountBlock = document.querySelector(".social__comment-count");
+const commentsShownCount = 5;
+const commentsLoader = bigPictureSection.querySelector(
+  ".social__comments-loader"
+);
+
 const userComments = await fetch("http://127.0.0.1:4001/comments")
   .then((response) => response.json())
   .then((json) => {
     return json;
   })
   .catch(() => alert("ERROR"));
+const commentsArray = userComments.slice();
 
 function openBigPictureWindow(e) {
   if (e.target.classList.contains("picture__img")) {
     const id = +e.target.dataset.id; //Указываю что айди фото это айди у картинки на которую мы кликаем
+    const commentsCount = +e.target.dataset.commentsCount;
     const photoInfo = userPhotos.find((e) => e.id === id); //Нахожу объект который соответствует айди фото
-    const image = bigPictureSection.querySelector("img"); //Нахожу тег img внутри секции с большим фото
-    const description = bigPictureSection.querySelector(".social__caption"); //Нахожу тег с классом social__caption внутри секции с большим фото
-    const likesCount = bigPictureSection.querySelector(".likes-count"); //Нахожу тег с классом likes-count внутри секции с большим фото
-    const commentsCount = bigPictureSection.querySelector(".comments-count"); //Нахожу тег с классом comments-count внутри секции с большим фото
-    const commentsCountBlock = bigPictureSection.querySelector(
-      ".social__comment-count"
-    ); //Нахожу тег с классом social__comment-count внутри секции с большим фото
 
     bigPictureSection.classList.remove("hidden"); //При открытии убираю класс для скрытия тега и добавляю класс к body, а также скрываю счетчик комментариев как сказано в задании
     body.classList.add("modal-open");
-    commentsCountBlock.classList.add("hidden"); //скрываю количество комментариев так как сказано в задании
 
     image.src = photoInfo.url;
     image.alt = photoInfo.description;
     description.textContent = photoInfo.description;
     likesCount.textContent = photoInfo.likes;
-    commentsCount.textContent = `10`;
-    getComments(photoInfo); //Указываю необходимые данные из объекта для правильного отображения и с помощью функции отображаю комментарии
+
+    getComments(commentsCount); //Указываю необходимые данные из объекта для правильного отображения и с помощью функции отображаю комментарии
+
+    return;
   }
 }
 
-function getComments(photoInfo) {
-  const commentsList = bigPictureSection.querySelector(".social__comments"); //Нахожу тег для вписывания элементов списка
+function getComments(commentsCount) {
+  if (commentsCount > 0) {
+    if (commentsCount <= 5) {
+      const comments = commentsArray.slice(0, commentsCount);
+
+      commentsList.innerHTML = ``; //Очищаю список от предыдущих комментариев если те были
+      commentsLoader.classList.add("hidden");
+      commentsShown.textContent = commentsCount;
+      commentsMaxCount.textContent = commentsCount;
+      createComments(comments);
+      return;
+    } else {
+      const preloadedComments = commentsArray.slice(0, 5);
+      const restComments = commentsArray.slice(5, commentsCount);
+
+      commentsList.innerHTML = ``; //Очищаю список от предыдущих комментариев если те были
+      commentsLoader.classList.remove("hidden");
+      commentsCountBlock.classList.remove("hidden");
+      commentsMaxCount.textContent = commentsCount;
+      commentsShown.textContent = commentsShownCount;
+
+      createComments(preloadedComments);
+      commentsLoader.addEventListener("click", function () {
+        loadComments(restComments, commentsCount);
+      });
+      return;
+    }
+  } else {
+    commentsList.innerHTML = ``; //Очищаю список от предыдущих комментариев если те были
+    commentsCountBlock.classList.add("hidden");
+    commentsLoader.classList.add("hidden");
+    return;
+  }
+}
+
+function loadComments(arr, commentsCount) {
+  const newArr = arr.splice(0, 5);
+  console.log(commentsArray);
+
+  console.log(restComments);
+  console.log(newArr);
+
+  if (commentsCount - commentsList.children.length <= 5) {
+    commentsLoader.classList.add("hidden");
+    createComments(newArr);
+
+    commentsShown.textContent = commentsList.children.length;
+
+    return;
+  } else {
+    commentsLoader.classList.remove("hidden");
+    createComments(newArr);
+
+    commentsShown.textContent = commentsList.children.length;
+    return;
+  }
+}
+
+function createComments(arr) {
   const documentFragment = document.createDocumentFragment(); //Создаю элемент для фрагмента кода
-  const comments = photoInfo.comments; //Передаю из параметров функции массив комментариев внутри объекта фото
-  commentsList.innerHTML = ``; //Очищаю список от предыдущих комментариев если те были
-  for (let i = 0; i < 10; i++) {
+
+  for (let i = 0; i < arr.length; i++) {
     const comment = document.createElement("li");
     comment.className = "social__comment";
     comment.innerHTML = `
     <img
      class="social__picture"
-     src=${userComments[i].avatar}
-     alt=${userComments[i].name}
+     src=${arr[i].avatar}
+     alt=${arr[i].name}
      width="35" height="35">
-   <p class="social__text">${userComments[i].message}</p>
+   <p class="social__text">${arr[i].message}</p>
     `;
     documentFragment.appendChild(comment);
   }
-
   commentsList.appendChild(documentFragment); //Добавляю сгенерированный фрагмент кода в тег для списка
+  return;
 }
 
 export { openBigPictureWindow };
--- photos-app-main/js/showMiniPictures.js
+++ photos-app-main/js/showMiniPictures.js
@@ -1,23 +1,36 @@
 //************ ОТОБРАЖЕНИЕ МИНИАТЮР ФОТО НА СТРАНИЦЕ ************/
 
 function showMiniPictures(arr) {
+  const commentsCountRules = {
+    minCount: 0,
+    maxCount: 40,
+  };
   const picturesBlock = document.querySelector(".pictures"); //нахожу блок в который вставлять фото
   const pictureTemplate = document.querySelector("#picture"); //нахожу шаблон с кодом для вставки фото
   const documentFragment = document.createDocumentFragment(); //создаю фрагмент документа, в который буду добавлять элементы фото
 
   arr.forEach((photo) => {
+    const commentsCount = getRandomNumber(
+      commentsCountRules.minCount,
+      commentsCountRules.maxCount
+    );
     const picture = pictureTemplate.content.cloneNode(true);
     const image = picture.querySelector(".picture__img");
     const comments = picture.querySelector(".picture__comments");
     const likes = picture.querySelector(".picture__likes");
     image.src = `${photo.url}`;
-    comments.textContent = `10`;
+    comments.textContent = commentsCount;
     likes.textContent = `${photo.likes}`;
     image.dataset.id = photo.id;
+    image.dataset.commentsCount = commentsCount;
     documentFragment.appendChild(picture); //все новые элементы добавляю в фрагмент документа который затем буду вставлять в блок для фото
   }); //перебираю элементы массива для того, чтобы после клонирования содержимого шаблона, можно было указать необходимые данные внутри контента
 
   picturesBlock.appendChild(documentFragment); //добавляю фрагмент документа в блок для фото для вывода всех фото
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 export { showMiniPictures };
