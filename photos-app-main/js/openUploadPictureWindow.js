098969d80a98af7dc7234e2eba258f98535e5668
Vlad <arkhipovvladyslav@gmail.com>
2023-07-05 17:03:34 +0200

    changes

--- photos-app-main/js/openUploadPictureWindow.js
+++ photos-app-main/js/openUploadPictureWindow.js
@@ -1,6 +1,7 @@
 //************ ОТОБРАЖЕНИЕ ЭКРАНА ДЛЯ ЗАГРУЗКИ НОВОГО ФОТО ************/
 import { formValidation } from "./formValidation.js";
 import { imageEffects } from "./imageEffects.js";
+const image = document.querySelector(".img-upload__preview"); //нахожу картинку
 function openUploadPictureWindow() {
   const formSubmitBtn = document.querySelector(".img-upload__submit"); //Нахожу кнопку для отправки формы с новым фото
   const uploadPictureBlock = document.querySelector(".img-upload__overlay");
@@ -8,6 +9,8 @@ function openUploadPictureWindow() {
 
   body.classList.add("modal-open");
   uploadPictureBlock.classList.remove("hidden");
+  image.style.transform = `scale(1)`;
+  image.style.filter = ``;
   imageEffects();
   formSubmitBtn.addEventListener("click", formValidation); //При нажатии на кнопку для отправки формы происходит валидация формы
 } //При добавлении фото меняются классы элементов бади и блока с формой для загрузки фото
