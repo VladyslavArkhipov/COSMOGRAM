31e9796c394f2fcee6d83788134c37ebf8f40047
Vlad <arkhipovvladyslav@gmail.com>
2023-07-07 18:12:49 +0200

    change

--- photos-app-main/js/formValidation.js
+++ photos-app-main/js/formValidation.js
@@ -41,7 +41,7 @@ function formValidation(e) {
               hashtagsInput.setCustomValidity("");
               hashtagsInput.reportValidity();
 
-              uploadFile(uploadFile);
+              uploadFile(uploadedPhoto);
             }
           }
         });
